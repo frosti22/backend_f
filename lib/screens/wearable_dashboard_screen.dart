269f9819871e391e39e9965739a9eee98cdb9793
frosti22 <jamesaldrinpabustan@gmail.com>
2026-08-19 01:12:28 +0800

    30 secs

--- lib/screens/wearable_dashboard_screen.dart
+++ lib/screens/wearable_dashboard_screen.dart
@@ -21,11 +21,7 @@ class _WearableDashboardScreenState extends State<WearableDashboardScreen>
   final ApiService _api = ApiService();
 
   // Avoid aggressive Health Connect polling/rate limiting.
-<<<<<<< HEAD
-  static const Duration _autoSyncInterval = Duration(seconds: 90);
-=======
   static const Duration _autoSyncInterval = Duration(seconds: 30);
->>>>>>> testbranch_4
 
   WearableSnapshot _snapshot = WearableSnapshot.empty();
 
